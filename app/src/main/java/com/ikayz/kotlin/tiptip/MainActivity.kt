5c69a7dcdd9669e49a26864ef1dd8b8f6f0a9629
Isaac Miti <ikayz@live.com>
2022-06-27 23:28:37 +0200

    Add binding

--- app/src/main/java/com/ikayz/kotlin/tiptip/MainActivity.kt
+++ app/src/main/java/com/ikayz/kotlin/tiptip/MainActivity.kt
@@ -2,8 +2,12 @@ package com.ikayz.kotlin.tiptip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ikayz.kotlin.tiptip.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
